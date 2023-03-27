033005fd21a13e542eb62175ab058c9bdffaa2c3
Sasha <alex_penza@list.ru>
2023-03-27 22:34:13 +0300

    origin

---
+++ src/main/java/com/example/hwcollections/HwcollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwcollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwcollectionsApplication.class, args);
+	}
+
+}
