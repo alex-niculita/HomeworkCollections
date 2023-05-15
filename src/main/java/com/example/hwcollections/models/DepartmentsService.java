45d9c8571a8e2cd1a26ce748135826a33ffe8f98
Sasha <alex_penza@list.ru>
2023-05-15 17:37:02 +0300

    Added commons-lang3 upd2

--- src/main/java/com/example/hwcollections/models/DepartmentsService.java
+++ src/main/java/com/example/hwcollections/models/DepartmentsService.java
@@ -3,7 +3,7 @@ package com.example.hwcollections.models;
 import java.util.List;
 import java.util.Map;
 
-public interface DepartmentService {
+public interface DepartmentsService {
     List<Employee> getDepartment(int departmentN);
     Employee findEmployeeWithMinSalaryForDept(int department);
     Employee findEmployeeWithMaxSalaryForDept(int department);
